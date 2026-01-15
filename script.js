2c3fea1ceee9c6ec36c43835d76c961b1275fc4b
Prahasta satria <70164021+vflawlest@users.noreply.github.com>
2026-01-15 22:16:35 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({ behavior: "smooth" });
+  });
+});
+
+// animasi
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+  reveals.forEach((el) => {
+    const windowHeight = window.innerHeight;
+    const elementTop = el.getBoundingClientRect().top;
+    const visiblePoint = 120;
+
+    if (elementTop < windowHeight - visiblePoint) {
+      el.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
